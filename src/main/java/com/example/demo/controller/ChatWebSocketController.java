51ea45df89ba6ba6824e44100c995d7cf117cbbe
sangwooeom <cdm19720@gmail.com>
2022-07-25 02:03:34 +0900

    변경사항 적용

--- src/main/java/com/example/demo/controller/ChatWebSocketController.java
+++ src/main/java/com/example/demo/controller/ChatWebSocketController.java
@@ -1,14 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatWebSocketController {
-	@MessageMapping("/request")
+	@MessageMapping("/request/{seq}")
 	@SendTo("/chatResponse/{seq}")
-	public String chatResponse(String message) {
+	public String chatResponse(String message, @DestinationVariable long seq) {
 		return message;
 	}
 }
